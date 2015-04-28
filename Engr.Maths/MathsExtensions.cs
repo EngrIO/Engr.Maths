7dd0f1c1bfd9d80e1b567f4b478f1f39a8b13f16
veggielane <chris@lane.org.uk>
2015-04-28 19:27:06 +0100

    Added Vect3

---
+++ Engr.Maths/MathsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engr.Maths
+{
+    public static class MathsExtensions
+    {
+        public static bool NearlyEquals(this Double x, Double y, Double epsilon = 0.0000001)
+        {
+            return Math.Abs(x - y) <= Math.Abs(x * .00001);
+        }
+        public static bool NearlyEquals(this float x, float y, float epsilon = 0.0000001f)
+        {
+            return Math.Abs(x - y) <= Math.Abs(x * .00001);
+        }
+    }
+}
